7c90622769004a3dcdb72ec88861738fe44dbaf4
lzle <lianzhilei0711@163.com>
2022-09-22 20:52:53 +0800

    插入排序

---
+++ algorithm/insertion/sort.py
@@ -0,0 +1,26 @@
+import random
+
+
+def insertion(list):
+    num = len(list)
+
+    for i in range(1, num):
+        curr = list[i]
+        flag = i
+        for j in range(i-1, -1, -1):
+            if curr >= list[j]:
+                break
+            list[j+1] = list[j]
+            flag = j
+
+        if flag != i :
+            list[flag] = curr
+
+
+list = []
+for i in range(1, 51):
+    list.append(i)
+
+random.shuffle(list)
+insertion(list)
+print(list)
