071ff285e37a8a88f7a65f4a489b225f11ad24b3
lzle <lianzhilei0711@163.com>
2022-09-24 17:49:43 +0800

    归并排序

---
+++ algorithm/merge/sort.py
@@ -0,0 +1,41 @@
+
+import random
+
+
+def merge(list, left, right):
+    if left < right:
+        mid = (left + right) // 2
+        merge(list, left, mid)
+        merge(list, mid+1, right)
+
+        tmp = []
+        i = left
+        j = mid + 1
+
+        while i <= mid and j <= right:
+            if list[i] <= list[j]:
+                tmp.append(list[i])
+                i += 1
+            else:
+                tmp.append(list[j])
+                j += 1
+        if i <= mid:
+            tmp.extend(list[i:mid+1])
+
+        if j <= right:
+            tmp.extend(list[j:right+1])
+
+        list[left:right+1] = tmp
+
+
+list = []
+for i in range(1, 51):
+    list.append(i)
+
+random.shuffle(list)
+merge(list, 0 , len(list)-1)
+print(list)
+
+
+
+
